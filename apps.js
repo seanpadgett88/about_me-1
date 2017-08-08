dd58d53579b5614055963d42a67063eac3a45155
Hewlette Dibble <hcodydibble@gmail.com>
2017-08-08 16:10:59 -0700

    added console.log commands

--- apps.js
+++ apps.js
@@ -1,8 +1,13 @@
 'use strict';
 
-var error = 'Hey now! That\'s not a real answer!';
+var error = 'Not to be bossy but I need a Y/N answer';
+var dogQ = 'Do I have a dog?';
+var spiritAnimalQ = 'Are cats my spirit animal?';
+var owlQ = 'Do I have an owl tattoo?';
+var wallsQ = 'Can I see through walls?';
+var cutMeQ = 'If you cut me, will I bleed?';
 
-var dog = prompt('Do I have a dog?').toUpperCase();
+var dog = prompt(dogQ).toUpperCase();
 if (dog == 'YES' || dog == 'Y') {
   alert('Sorry friend, I\'m a cat person through and through');
 }else if (dog == 'NO' || dog == 'N') {
@@ -10,8 +15,10 @@ if (dog == 'YES' || dog == 'Y') {
 }else {
   alert(error);
 }
+console.log('Q: ' + dogQ);
+console.log('A: ' + dog);
 
-var spiritAnimal = prompt('Are cats my spirit animal?').toUpperCase();
+var spiritAnimal = prompt(spiritAnimalQ).toUpperCase();
 if (spiritAnimal == 'YES' || spiritAnimal == 'Y') {
   alert('Incorrect! I, oddly enough, feel like a bear is my spirit animal');
 }else if (spiritAnimal == 'NO' || spiritAnimal == 'N') {
@@ -19,8 +26,10 @@ if (spiritAnimal == 'YES' || spiritAnimal == 'Y') {
 }else {
   alert(error);
 }
+console.log('Q: ' + spiritAnimalQ);
+console.log('A: ' + spiritAnimal);
 
-var owl = prompt('Do I have an owl tattoo?').toUpperCase();
+var owl = prompt(owlQ).toUpperCase();
 if (owl == 'YES' || owl == 'Y') {
   alert('Heck yes I do! It is small and on my finger!');
 }else if (owl == 'NO' || owl == 'N') {
@@ -28,8 +37,10 @@ if (owl == 'YES' || owl == 'Y') {
 }else {
   alert(error);
 }
+console.log('Q: ' + owlQ);
+console.log('A: ' + owl);
 
-var walls = prompt('Can I see through walls?').toUpperCase();
+var walls = prompt(wallsQ).toUpperCase();
 if (walls == 'YES' || walls == 'Y') {
   alert('I WISH! Unfortunately I am neither a super hero nor magical');
 }else if (walls == 'NO' || walls == 'N') {
@@ -37,3 +48,16 @@ if (walls == 'YES' || walls == 'Y') {
 }else {
   alert(error);
 }
+console.log('Q: ' + wallsQ);
+console.log('A: ' + walls);
+
+var cutMe = prompt(cutMeQ).toUpperCase();
+if (cutMe == 'YES' || cutMe == 'Y') {
+  alert('You\'re right! Please don\'t do it');
+}else if (cutMe == 'NO' || cutMe == 'N') {
+  alert('That is wrong for I am only human');
+}else {
+  alert(error);
+}
+console.log('Q: ' + cutMeQ);
+console.log('A: ' + cutMe);
