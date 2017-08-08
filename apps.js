f37118f40de6d1b18f3f310b6bf616a977b09b6a
Hewlette Dibble <hcodydibble@gmail.com>
2017-08-08 15:55:18 -0700

    added four variables w questions

--- apps.js
+++ apps.js
@@ -1,3 +1,39 @@
 'use strict';
 
-var dog = prompt('Do I have a pet dog?');
+var error = 'Hey now! That\'s not a real answer!';
+
+var dog = prompt('Do I have a dog?').toUpperCase();
+if (dog == 'YES' || dog == 'Y') {
+  alert('Sorry friend, I\'m a cat person through and through');
+}else if (dog == 'NO' || dog == 'N') {
+  alert('You are correct! Cats rule and dogs drool');
+}else {
+  alert(error);
+}
+
+var spiritAnimal = prompt('Are cats my spirit animal?').toUpperCase();
+if (spiritAnimal == 'YES' || spiritAnimal == 'Y') {
+  alert('Incorrect! I, oddly enough, feel like a bear is my spirit animal');
+}else if (spiritAnimal == 'NO' || spiritAnimal == 'N') {
+  alert('That\'s right! I identify more with a bear');
+}else {
+  alert(error);
+}
+
+var owl = prompt('Do I have an owl tattoo?').toUpperCase();
+if (owl == 'YES' || owl == 'Y') {
+  alert('Heck yes I do! It is small and on my finger!');
+}else if (owl == 'NO' || owl == 'N') {
+  alert('Sorry friend but that isn\'t correct, I have one on my finger');
+}else {
+  alert(error);
+}
+
+var walls = prompt('Can I see through walls?').toUpperCase();
+if (walls == 'YES' || walls == 'Y') {
+  alert('I WISH! Unfortunately I am neither a super hero nor magical');
+}else if (walls == 'NO' || walls == 'N') {
+  alert('Sadly you are correct, I have no such abilities');
+}else {
+  alert(error);
+}
