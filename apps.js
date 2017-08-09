7750ffb8c56d3da8a9bbd4aa9fd9ea4545a72ee0
Hewlette Dibble <hcodydibble@gmail.com>
2017-08-08 21:32:11 -0700

    changed to strict true and added the option for YEP and NOPE

--- apps.js
+++ apps.js
@@ -8,9 +8,9 @@ var wallsQ = 'Can I see through walls?';
 var cutMeQ = 'If you cut me, will I bleed?';
 
 var dog = prompt(dogQ).toUpperCase();
-if (dog == 'YES' || dog == 'Y') {
+if (dog === 'YES' || dog === 'Y' || dog === 'YEP') {
   alert('Sorry friend, I\'m a cat person through and through');
-}else if (dog == 'NO' || dog == 'N') {
+}else if (dog === 'NO' || dog === 'N' || dog === 'NOPE') {
   alert('You are correct! Cats rule and dogs drool');
 }else {
   alert(error);
@@ -19,9 +19,9 @@ console.log('Q: ' + dogQ);
 console.log('A: ' + dog);
 
 var spiritAnimal = prompt(spiritAnimalQ).toUpperCase();
-if (spiritAnimal == 'YES' || spiritAnimal == 'Y') {
+if (spiritAnimal === 'YES' || spiritAnimal === 'Y' || spiritAnimal === 'YEP') {
   alert('Incorrect! I, oddly enough, feel like a bear is my spirit animal');
-}else if (spiritAnimal == 'NO' || spiritAnimal == 'N') {
+}else if (spiritAnimal === 'NO' || spiritAnimal === 'N' || spiritAnimal === 'NOPE') {
   alert('That\'s right! I identify more with a bear');
 }else {
   alert(error);
@@ -30,9 +30,9 @@ console.log('Q: ' + spiritAnimalQ);
 console.log('A: ' + spiritAnimal);
 
 var owl = prompt(owlQ).toUpperCase();
-if (owl == 'YES' || owl == 'Y') {
+if (owl === 'YES' || owl === 'Y' || owl === 'YEP') {
   alert('Heck yes I do! It is small and on my finger!');
-}else if (owl == 'NO' || owl == 'N') {
+}else if (owl === 'NO' || owl === 'N' || owl === 'NOPE') {
   alert('Sorry friend but that isn\'t correct, I have one on my finger');
 }else {
   alert(error);
@@ -41,9 +41,9 @@ console.log('Q: ' + owlQ);
 console.log('A: ' + owl);
 
 var walls = prompt(wallsQ).toUpperCase();
-if (walls == 'YES' || walls == 'Y') {
+if (walls === 'YES' || walls === 'Y' || walls === 'YEP') {
   alert('I WISH! Unfortunately I am neither a super hero nor magical');
-}else if (walls == 'NO' || walls == 'N') {
+}else if (walls === 'NO' || walls === 'N' || walls === 'NOPE') {
   alert('Sadly you are correct, I have no such abilities');
 }else {
   alert(error);
@@ -52,9 +52,9 @@ console.log('Q: ' + wallsQ);
 console.log('A: ' + walls);
 
 var cutMe = prompt(cutMeQ).toUpperCase();
-if (cutMe == 'YES' || cutMe == 'Y') {
+if (cutMe === 'YES' || cutMe === 'Y' || cutMe === 'YEP') {
   alert('You\'re right! Please don\'t do it');
-}else if (cutMe == 'NO' || cutMe == 'N') {
+}else if (cutMe === 'NO' || cutMe === 'N' || cutMe === 'NOPE') {
   alert('That is wrong for I am only human');
 }else {
   alert(error);
