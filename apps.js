f41615162f86d2c76303c1b134e6b1e99af9e811
Hewlette Dibble <hcodydibble@gmail.com>
2017-08-09 13:42:51 -0700

    Added arrays and switched answers to said arrays

--- apps.js
+++ apps.js
@@ -6,11 +6,13 @@ var spiritAnimalQ = 'Are cats my spirit animal?';
 var owlQ = 'Do I have an owl tattoo?';
 var wallsQ = 'Can I see through walls?';
 var cutMeQ = 'If you cut me, will I bleed?';
+var questions = [dogQ, spiritAnimalQ, owlQ, wallsQ, cutMeQ];
+var answers = ['YES', 'Y', 'YEP', 'YEA', 'NO', 'N', 'NOPE', 'NAH'];
 
-var dog = prompt(dogQ).toUpperCase();
-if (dog === 'YES' || dog === 'Y' || dog === 'YEP') {
+var dog = prompt(questions[0]);
+if (dog.toUpperCase() === answers[0] || answers[1] || answers[2] || answers[3]) {
   alert('Sorry friend, I\'m a cat person through and through');
-}else if (dog === 'NO' || dog === 'N' || dog === 'NOPE') {
+}else if (dog.toUpperCase() === answers[4] || answers[5] || answers[6] || answers[7]) {
   alert('You are correct! Cats rule and dogs drool');
 }else {
   alert(error);
@@ -18,10 +20,10 @@ if (dog === 'YES' || dog === 'Y' || dog === 'YEP') {
 console.log('Q: ' + dogQ);
 console.log('A: ' + dog);
 
-var spiritAnimal = prompt(spiritAnimalQ).toUpperCase();
-if (spiritAnimal === 'YES' || spiritAnimal === 'Y' || spiritAnimal === 'YEP') {
+var spiritAnimal = prompt(questions[1]).toUpperCase();
+if (spiritAnimal.toUpperCase() === answers[0]) || answers[1] || answers[2] || answers[3]) {
   alert('Incorrect! I, oddly enough, feel like a bear is my spirit animal');
-}else if (spiritAnimal === 'NO' || spiritAnimal === 'N' || spiritAnimal === 'NOPE') {
+}else if (spiritAnimal.toUpperCase() === answers[4] || answers[5] || answers[7] || answers[7]) {
   alert('That\'s right! I identify more with a bear');
 }else {
   alert(error);
@@ -29,10 +31,10 @@ if (spiritAnimal === 'YES' || spiritAnimal === 'Y' || spiritAnimal === 'YEP') {
 console.log('Q: ' + spiritAnimalQ);
 console.log('A: ' + spiritAnimal);
 
-var owl = prompt(owlQ).toUpperCase();
-if (owl === 'YES' || owl === 'Y' || owl === 'YEP') {
+var owl = prompt(questions[2]).toUpperCase();
+if (owl.toUpperCase() === answers[0] || answers[1] || answers[2] || answers[3]) {
   alert('Heck yes I do! It is small and on my finger!');
-}else if (owl === 'NO' || owl === 'N' || owl === 'NOPE') {
+}else if (owl === answers[4] || answers[5] || answers[6] || answers[7]) {
   alert('Sorry friend but that isn\'t correct, I have one on my finger');
 }else {
   alert(error);
@@ -40,10 +42,10 @@ if (owl === 'YES' || owl === 'Y' || owl === 'YEP') {
 console.log('Q: ' + owlQ);
 console.log('A: ' + owl);
 
-var walls = prompt(wallsQ).toUpperCase();
-if (walls === 'YES' || walls === 'Y' || walls === 'YEP') {
+var walls = prompt(questions[3]).toUpperCase();
+if (walls === answers[0] || answers[1] || answers[2] || answers[3]) {
   alert('I WISH! Unfortunately I am neither a super hero nor magical');
-}else if (walls === 'NO' || walls === 'N' || walls === 'NOPE') {
+}else if (walls === answers[4] || answers[5] || answers[6] || answers[7]) {
   alert('Sadly you are correct, I have no such abilities');
 }else {
   alert(error);
@@ -51,10 +53,10 @@ if (walls === 'YES' || walls === 'Y' || walls === 'YEP') {
 console.log('Q: ' + wallsQ);
 console.log('A: ' + walls);
 
-var cutMe = prompt(cutMeQ).toUpperCase();
-if (cutMe === 'YES' || cutMe === 'Y' || cutMe === 'YEP') {
+var cutMe = prompt(questions[4]).toUpperCase();
+if (cutMe === answers[0] || answers[1] || answers[2] || answers[3]) {
   alert('You\'re right! Please don\'t do it');
-}else if (cutMe === 'NO' || cutMe === 'N' || cutMe === 'NOPE') {
+}else if (cutMe === answers[4] || answers[5] || answers[6] || answers[7]) {
   alert('That is wrong for I am only human');
 }else {
   alert(error);
